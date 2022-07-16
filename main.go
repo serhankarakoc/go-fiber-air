a0b29cf92e18907fa223e448a0dd630ff3dfc2ec
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2022-07-16 12:06:45 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,14 +7,11 @@ import (
 )
 
 func main() {
-	// Create new Fiber instance
 	app := fiber.New()
 
-	// Create new GET route on path "/hello"
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	// Start server on http://localhost:3000
 	log.Fatal(app.Listen(":3000"))
 }
